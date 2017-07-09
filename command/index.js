63e98b5d8452694bd2d2fc8716aca306cf92505b
fangwentian <654396466@qq.com>
2017-07-09 21:28:51 +0800

    update yargs

--- command/index.js
+++ command/index.js
@@ -3,16 +3,21 @@
 const yargs = require('yargs');
 const main = require('../core/main')
 const argv = yargs
-    .usage('Usage: pop toolkit [options]')
-    .alias('u', 'url')
-    .describe('u', 'page url')
-    .alias('p', 'project')
-    .describe('p', 'project name')
-    .alias('f', 'force')
+    .usage('Usage: pop toolkit [options], you must hava a popt.config folder to store the config files.')
+    .option('url', {
+        alias: 'u',
+        describe: 'page url, such as "marketing/coupon"'
+    })
+    .option('project', {
+        alias: 'p',
+        describe: 'project name, such as "pop"、"cmms"'
+    })
+    .alias('force', 'f')
     .boolean('f')
     .describe('f', 'force to override files if exist')
+    .alias('help', 'h')
+    .demandOption(['url', 'project'], 'Please provide both url and project arguments to work with this tool')
     .help('h')
-    .alias('h', 'help')
     .argv;
 
 let {url, project, force} = argv;
